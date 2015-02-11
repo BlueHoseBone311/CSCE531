491f0e1d2746590ce9363e5b358b01a4fdc23ee7
Christian Merchant <bluehosebone311@yahoo.com>
2015-02-10 21:53:58 -0500

    hashtbl update

--- hashtbl.c
+++ hashtbl.c
@@ -32,26 +32,43 @@ void init_dict()
 // Add a key with an integer constant value to the dictionary
 void add_int_to_dict(const char *key, long val)
 {
-    DR p = (DR) malloc(sizeof(DICT_REC));
-    p->in_cycle = FALSE;
-    p->key = key;
-    p->tag = INT_CONST;
-    p->u.intconstval = val;
-    if (insert_or_update(p) == 0)
-        fprintf(stderr, "Warning: redefinition of %s to %ld at line %d\n",
-                key, val, line_no);
+    DR entry = (DR) malloc(sizeof(DICT_REC));
+    entry->in_cycle = FALSE;
+    entry->key = key;
+    entry->tag = INT_CONST;
+    entry->u.intconstval = val;
+    if (insert_or_update(entry) == 0)
+    {    
+        fprintf(stderr, "Warning: redefinition of %s to %ld at line %d\n",key, val, line_no);
+    }    
 }
 
 // Add a key with a string constant value to the dictionary
 void add_str_to_dict(const char *key, const char *val)
 {
-        // redacted
+    DR entry = (DR) malloc(sizeof(DICT_REC));
+    entry->in_cycle = FALSE;
+    entry->key = key;
+    entry->tag = STR_CONST;
+    entry->u.strconstval = val;
+    if (insert_or_update(entry) == 0)
+    {    
+        fprintf(stderr, "Warning: redefinition of %s to %ld at line %d\n",key, val, line_no);
+    }
 }
 
 // Add a key with an identifier value to the dictionary
 void add_id_to_dict(const char *key, const char *val)
 {
-        // redacted
+    DR entry = (DR) malloc(sizeof(DICT_REC));
+    entry->in_cycle = FALSE;
+    entry->key = key;
+    entry->tag = INT_CONST;
+    entry->u.idval = val;
+    if (insert_or_update(entry) == 0)
+    {    
+        fprintf(stderr, "Warning: redefinition of %s to %ld at line %d\n",key, val, line_no);
+    }    
 }
 
 // Output the substituted value of a defined ID to the output stream
@@ -62,3 +79,102 @@ void output_substitution(FILE *stream, const char *id)
 
 
 /* Local routines */
+DR get_item(const char *key)
+{   
+    int index = hash(key);
+    DR *entry;
+    for (entry = hash_tab[index]; entry != NULL; entry = entry->next)
+    {
+        if (strcmp(key, entry->key) == 0)
+        {
+            return entry; 
+        }       
+    }    
+    return NULL; 
+}
+
+int insert_or_update(DR new_item);
+{
+    int status = 0;
+    int index = hash(new_item->key);
+    char * id_val = ID; 
+    DR *curr_item;
+
+    if ((curr_item = get_item(new_item->key)) != NULL) /*a key exists that is the same as the new item*/
+    {
+        cur_item->tag = new_item->tag
+        cur_item->u = new_item->u;
+        if (strcmp(curr_item->tag,id_val) == 0)
+        {
+            check_or_mark_cycle(curr_item);  
+        } 
+    }
+    else if (hash_tab[index] = NULL) /*this bucket has not been hashed to - fill it */ 
+    {
+       int hashval = hash(index);
+       new_item->next = hash_tab[hashval];
+       hash_tab[hashval] = new_item;
+       if (strcmp(new_item->tag,id_val) == 0)
+       {
+            check_or_mark_cycle(new_item);  
+       }
+       ++num_items; 
+       status = 1; 
+    }
+    else /*a key exists but it is not the same as the new_item key - add to end*/
+    {
+        DR *ptr = hash_tab[index];
+        while(ptr->next != NULL)
+        {
+            ptr = ptr->next; 
+        } 
+        ptr->next = new_item;
+        if (strcmp(curr_item->tag,id_val) == 0)
+        {
+            check_or_mark_cycle(curr_item);  
+        }
+        ++num_items; 
+        status = 1;     
+    }
+
+    return status;   
+}
+void check_or_mark_cycle(DR item);
+void unmark_cycle(DR item);
+int hash(const char *key)
+{
+    int sum = 0;
+    for (; *key; key++)
+    {    
+        sum = (37*sum + *key) % h_size;
+    }
+        
+    return sum;
+}
+void insert_at_front(DR *list, DR new_item);
+DR remove_from_front(DR *list);
+void resize(int size)
+{ 
+    DR *new_table = (DR *) malloc(size*sizeof(DICT_REC)); 
+
+    if (hash_tab == NULL) /*initial dictionary*/
+    { 
+        for (int i = 0; i <size; i++)
+        {
+           new_table[i] = NULL;
+        }
+        hash_tab = new_table; 
+        h_size = size; 
+    } 
+    else
+    {
+        while(int i < h_size)
+        {
+            DR *entry;
+            for (entry = hash_tab[i]; entry != NULL; entry = entry->next)
+            {   
+                hash(entry->key
+            }       
+        }    
+    }       
+}
